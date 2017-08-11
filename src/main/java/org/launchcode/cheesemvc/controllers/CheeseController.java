b33c5f3614d2c71f02f83af95ffd6fae67378d6c
Wren Bareilles <wren.ellbee@gmail.com>
2017-08-10 20:23:30 -0700

    Added option to remove cheeses.

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value="cheese")
@@ -36,5 +37,32 @@ public class CheeseController {
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc) {
         cheeses.put(cheeseName, cheeseDesc);
         return "redirect:";
+    }
+
+    // TODO: Write method @ GET to display a remove form ("remove")
+
+    @RequestMapping(value="remove", method=RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Remove Cheeses");
+        return "cheese/remove";
+    }
+
+    // TODO: Write method @ POST to remove a HashMap from ArrayList
+    @RequestMapping(value="remove", method=RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam String removeCheese) {
+        // Use a for loop to iterate over the hashmaps arraylist
+        // for (HashMap<String, String> cheese : cheeses) {
+            // Use a for loop to iterate over the keys in the hashmap
+        for (Map.Entry<String, String> cheeseToRemove : cheeses.entrySet()) {
+            String cheeseName = cheeseToRemove.getKey();
+            if (cheeseName.contains(removeCheese)) {
+                cheeses.remove(cheeseName);
+            }
+        }
+        return "redirect:";
+
+
+        // Redirect to index
     }
 }
